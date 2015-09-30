a153420ed88be1a0eb3bb38468853c4f82cff5ff
Colin Trainer <colin.trainer@iqx.co.uk>
2015-09-30 12:05:26 +0100

    Adding routing for Job Search

--- cliControllers/routes.js
+++ cliControllers/routes.js
@@ -13,6 +13,8 @@ angular.module('app').config(function($routeProvider) {
   .when('/canddiary',{controller:'CandDiaryCtrl',templateUrl:'/views/canddiary.html'})
   .when('/apiprocs',{controller:'APIprocsCtrl',templateUrl:'/views/apiProcs.html'})
   .when('/canddocuments',{controller:'CandDocsCtrl',templateUrl:'/views/canddocuments.html'})
+  .when('/jobsearch',{controller:'JobSearchCtrl',templateUrl:'/views/jobsearch.html'})
+
   })
   .run( function($rootScope, $location, ApplicationSvc) {
     // Register listener to watch route changes
