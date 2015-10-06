9d276cab5f1c6baf4b84c1db5bcf6088ae37639e
MichaelFHScott <mhs@iqx.co.uk>
2015-10-06 10:15:10 +0100

    Installer tweak and remove 'Logged in as'

--- install.bat
+++ install.bat
@@ -1,8 +1,9 @@
 @echo off
 set NODE_ENV=production
+call npm install
 call npm install -g nodemon
 call npm install -g node-windows
-call npm install
+call npm link node-windows
 echo ==================================================
 echo IQXWeb.bat will run the server in a console window
 echo node serviceInstall.js will install the service
